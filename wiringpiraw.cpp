05b4016b708a528c9f050ef301cf341796fc3045
ductran17 <mrductran173@gmail.com>
2023-03-02 01:15:48 +0700

    deem,khong,co,nghi add

--- wiringpiraw.cpp
+++ wiringpiraw.cpp
@@ -94,7 +94,7 @@ int main()
                                         {
                                             dataNum += 1;
                                             FILE *fptr;
-                                            string fileNameStr = "dataRaw/ddem_add/ddem_add" + to_string(dataNum) + ".csv";
+                                            string fileNameStr = "dataRaw/nghi_add/nghi_add" + to_string(dataNum) + ".csv";
                                             const char *fileName = fileNameStr.c_str();
                                             usleep(1000000);
                                             // sprintf(fileName, "dataRaw/tamws/tamws%d.csv", dataNum);
