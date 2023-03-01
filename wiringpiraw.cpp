c43a661a89ff7d90c92ae4f5550a8c4aee79a42a
ductran17 <mrductran173@gmail.com>
2023-03-01 11:49:14 +0700

    giayf,deps

--- wiringpiraw.cpp
+++ wiringpiraw.cpp
@@ -94,7 +94,7 @@ int main()
                                         {
                                             dataNum += 1;
                                             FILE *fptr;
-                                            string fileNameStr = "dataRaw/tamws/tamws" + to_string(dataNum) + ".csv";
+                                            string fileNameStr = "dataRaw/deps/deps" + to_string(dataNum) + ".csv";
                                             const char *fileName = fileNameStr.c_str();
                                             usleep(1000000);
                                             // sprintf(fileName, "dataRaw/tamws/tamws%d.csv", dataNum);
