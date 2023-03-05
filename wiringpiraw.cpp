6714f4b81333578e318d2e66f3c54f9231586f90
ductran17 <mrductran173@gmail.com>
2023-03-05 23:30:46 +0700

    datnuocx2add

--- wiringpiraw.cpp
+++ wiringpiraw.cpp
@@ -98,7 +98,7 @@ int main()
                                         {
                                             dataNum += 1;
                                             FILE *fptr;
-                                            string fileNameStr = "dataRaw/dienthoai_add_add_add/dienthoai_add_add_add" + to_string(dataNum) + ".csv";
+                                            string fileNameStr = "dataRaw/datnuoc_add_add/datnuoc_add_add" + to_string(dataNum) + ".csv";
                                             const char *fileName = fileNameStr.c_str();
                                             usleep(1000000);
                                             // sprintf(fileName, "dataRaw/tamws/tamws%d.csv", dataNum);
