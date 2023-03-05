5f914a9a0b35cc1d8ab16d1987811bca76025948
ductran17 <mrductran173@gmail.com>
2023-03-05 22:45:08 +0700

    xinchao x3add

--- wiringpiraw.cpp
+++ wiringpiraw.cpp
@@ -98,7 +98,7 @@ int main()
                                         {
                                             dataNum += 1;
                                             FILE *fptr;
-                                            string fileNameStr = "dataRaw/camon_add_add/camon_add_add" + to_string(dataNum) + ".csv";
+                                            string fileNameStr = "dataRaw/xinchao_add_add_add/xinchao_add_add_add" + to_string(dataNum) + ".csv";
                                             const char *fileName = fileNameStr.c_str();
                                             usleep(1000000);
                                             // sprintf(fileName, "dataRaw/tamws/tamws%d.csv", dataNum);
