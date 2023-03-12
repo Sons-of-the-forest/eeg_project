f6aa1dd290b86be8837515438620a2be2c517bd9
ductran17 <mrductran173@gmail.com>
2023-03-12 23:00:44 +0700

    newonefive

--- wiringpiraw.cpp
+++ wiringpiraw.cpp
@@ -17,7 +17,7 @@ int betaMessage = 0;
 int totalMessage = 0;
 int trueLengthMessage = 0;
 int interruptNum = 0;
-int dataNum = 0; // Increase this number to write new files
+int dataNum = 50; // Increase this number to write new files
 int brainwavePayload[3000] = {0};
 int firstBytePayload[3000] = {0};
 int secondBytePayload[3000] = {0};
@@ -98,7 +98,7 @@ int main()
                                         {
                                             dataNum += 1;
                                             FILE *fptr;
-                                            string fileNameStr = "dataRaw/datnuoc_new/datnuoc_new" + to_string(dataNum) + ".csv";
+                                            string fileNameStr = "dataRaw/tambiet_new/tambiet_new" + to_string(dataNum) + ".csv";
                                             const char *fileName = fileNameStr.c_str();
                                             usleep(1000000);
                                             // sprintf(fileName, "dataRaw/tamws/tamws%d.csv", dataNum);
